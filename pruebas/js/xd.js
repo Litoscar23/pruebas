aa4798775c05ec028456e02de751043e4567ebe9
Litoscar23 <carlosfer10112003@gmail.com>
2025-05-18 23:27:43 -0500

    Add files via upload

---
+++ pruebas/js/xd.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const boton = document.getElementById("miBoton");
+
+    boton.addEventListener('click', function() {
+        document.getElementById("chupala").textContent = " CHUPALA XD ";
+    });
+});
